a3480fe260559f3b757d323a713e1bf1bb2c8386
Yaten <yaten598@gmail.com>
2025-09-10 16:59:02 +0530

    add react-router dependency + initialize auth routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<p>This page is publicly accessible.</p>} />
+          <Route
+            path="/public"
+            element={
+              <SignedOut>
+                <p>You are signed out.</p>
+              </SignedOut>
+            }
+          />
+          <Route
+            path="/private"
+            element={
+              <SignedIn>
+                <p>You are signed in.</p>
+              </SignedIn>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
+  </React.StrictMode>,
 );
