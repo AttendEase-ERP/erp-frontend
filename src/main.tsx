bb5cf0dd7a73a4e80f61456d1cdfd2df6e407b70
Yaten <yaten598@gmail.com>
2025-09-11 19:51:47 +0530

    init: chakra-ui library

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { Provider } from "./components/ui/provider";
+import SignIn from "./components/auth/SignIn";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,28 +13,29 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<p>This page is publicly accessible.</p>} />
-          <Route
-            path="/public"
-            element={
-              <SignedOut>
-                <p>You are signed out.</p>
-              </SignedOut>
-            }
-          />
-          <Route
-            path="/private"
-            element={
-              <SignedIn>
-                <p>You are signed in.</p>
-              </SignedIn>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </ClerkProvider>
+    <Provider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/public"
+              element={
+                <SignedOut>
+                  <SignIn />
+                </SignedOut>
+              }
+            />
+            <Route
+              path="/private"
+              element={
+                <SignedIn>
+                  <p>You are signed in.</p>
+                </SignedIn>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ClerkProvider>
+    </Provider>
   </React.StrictMode>,
 );
