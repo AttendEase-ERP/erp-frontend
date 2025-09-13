87063252f2454c36c420a868b24bd5b13bf8aaae
Yaten <yaten598@gmail.com>
2025-09-13 11:00:15 +0530

    add sign-out option

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,16 +1,25 @@
-import { SignedIn, SignedOut } from "@clerk/react-router";
+import { Button } from "@chakra-ui/react";
+import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
 import { Navigate } from "react-router";
 
 export default function ProtectedRoute({
   children,
 }: {
-  children?: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <>
       <SignedIn>
-        <div>{children}</div>
+        <div>
+          {children}
+          <div style={{ marginTop: "1rem" }}>
+            <SignOutButton redirectUrl="/sign-in">
+              <Button>Sign out</Button>
+            </SignOutButton>
+          </div>
+        </div>
       </SignedIn>
+
       <SignedOut>
         <Navigate to="/sign-in" replace />
       </SignedOut>
