42ce4906bb655138438b2ae7caf5c72a607ff85f
Yaten <yaten598@gmail.com>
2025-10-27 07:13:22 +0000

    update idb users api to remove the email param (since there'll be only 1 record in idb for 1 user)

--- src/lib/idb/user/queries.ts
+++ src/lib/idb/user/queries.ts
@@ -2,11 +2,13 @@ import { idb } from "../index";
 import type { UserDetails } from "../types";
 
 export async function saveUserDetails(user: UserDetails) {
-  await idb.userDetails.put(user);
+  await idb.userDetails.clear();
+  await idb.userDetails.add(user);
 }
 
-export async function getUserDetails(email: string) {
-  return idb.userDetails.get(email);
+export async function getUserDetails() {
+  const allUsers = await idb.userDetails.toArray();
+  return allUsers[0];
 }
 
 export async function clearUserDetails() {
